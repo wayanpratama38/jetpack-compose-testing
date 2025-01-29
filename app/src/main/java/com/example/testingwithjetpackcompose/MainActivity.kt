a311c6a55bff27bdef143f34d32aa878500e2eaf
wynprtm <wayanpratama38@gmail.com>
2025-01-30 01:47:25 +0700

    Creating Simple Calculator

--- app/src/main/java/com/example/testingwithjetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/testingwithjetpackcompose/MainActivity.kt
@@ -1,47 +1,103 @@
 package com.example.testingwithjetpackcompose
 
 import android.os.Bundle
+import android.widget.Space
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.key
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.testingwithjetpackcompose.ui.theme.TestingWithJetpackComposeTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
             TestingWithJetpackComposeTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                CalculatorApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun CalculatorApp(
+    modifier : Modifier = Modifier
+){
+    var lengthInput by remember { mutableStateOf("") }
+    var widthInput by remember { mutableStateOf("") }
+    var result by remember { mutableStateOf("") }
+
+    val length = lengthInput.toDoubleOrNull() ?: 0.0
+    val width = widthInput.toDoubleOrNull() ?: 0.0
+
+    Box(
+        modifier = modifier.fillMaxSize().padding(16.dp),
+        contentAlignment = Alignment.Center
+    ){
+        Column {
+            TextField(
+                value = lengthInput,
+                onValueChange = {lengthInput = it},
+                label = {
+                    Text(stringResource(R.string.enter_length))
+                },
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+            )
+            Spacer(modifier.height(8.dp))
+            TextField(
+                value = widthInput,
+                onValueChange = {widthInput = it},
+                label = {
+                    Text(stringResource(R.string.enter_width))
+                },
+                singleLine = true,
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+            )
+            Button(
+                onClick = {
+                    result = (length * width).toString()
+                }
+            ) {
+                Text(
+                    text = stringResource(R.string.count)
+                )
+            }
+            Text(
+                text = stringResource(R.string.result,result)
+            )
+        }
+    }
+
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+@Preview(showBackground = true, device = Devices.PIXEL_4)
+fun CalculatorAppPreview(){
     TestingWithJetpackComposeTheme {
-        Greeting("Android")
+        CalculatorApp()
     }
 }
